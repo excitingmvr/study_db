d7180be9df1e44b87271602d9bcc0b3e47e7eeb6
kordelee <kordelee@naver.com>
2023-07-20 09:45:48 +0900

    Update cate.sql

--- mysql/exc/cate.sql
+++ mysql/exc/cate.sql
@@ -14,3 +14,23 @@ select * from infrCate where ifctDepth = 2 and ifctParents=4;
  select ifctSeq, ifctParents, cast('' as char(30)), ifctName, 0, cast(ifctSeq as char(10)), ifctOrder, ifctDepth from infrCate where ifctParents is null;
  
  select d.ifctSeq, d.ifctParents, cte.ifctName, d.ifctName, depth + 1, concat(cte.orderString, '-', d.ifctOrder), d.ifctOrder, d.ifctDepth from infrCate d inner join CteCate cte on d.ifctParents = cte.ifctSeq;
+
+--
+use nct;
+
+select * from infrCategory;
+
+WITH RECURSIVE CteCate(ifctSeq, ifctParents, ParentsName, ifctName, depth, orderString, ifctOrder, ifctDepth) AS 
+(
+    select ifctSeq, ifctParents, cast('' as char(30)), ifctName, 0, cast(ifctSeq as char(10)), ifctOrder, ifctDepth from infrCategory where ifctParents = 0 
+    UNION ALL
+    select d.ifctSeq, d.ifctParents, cte.ifctName, d.ifctName, depth + 1, concat(cte.orderString, '-', d.ifctOrder), d.ifctOrder, d.ifctDepth from infrCategory d inner join CteCate cte on d.ifctParents = cte.ifctSeq
+)
+select * from CteCate order by orderString;
+
+select * from infrCategory where ifctDepth = 2 and ifctParents=4;
+
+
+ select ifctSeq, ifctParents, cast('' as char(30)), ifctName, 0, cast(ifctSeq as char(10)), ifctOrder, ifctDepth from infrCategory where ifctParents = 0;
+ 
+ select d.ifctSeq, d.ifctParents, cte.ifctName, d.ifctName, depth + 1, concat(cte.orderString, '-', d.ifctOrder), d.ifctOrder, d.ifctDepth from infrCategory d inner join CteCate cte on d.ifctParents = cte.ifctSeq;
